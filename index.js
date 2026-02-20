9dbbc57d6827d79d5c8a851f243fa3a3695b4399
Ryan Ford <59795228+ryanmford@users.noreply.github.com>
2026-02-20 03:44:49 -1000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useEffect, useCallback } from 'react';
-import { createRoot } from 'react-dom/client';
+
 // --- CUSTOM STYLES ---
 const CustomStyles = () => (
   <style>{`
