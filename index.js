d0d6f822fe63347302c37050a59f50968ca74d6d
Ryan Ford <59795228+ryanmford@users.noreply.github.com>
2026-03-06 21:33:04 -1000

    Update index.js

--- index.js
+++ index.js
@@ -2383,15 +2383,3 @@ if (rootElement) {
     </React.StrictMode>
   );
 }
-                          // --- MOUNTING ---
-import ReactDOM from 'react-dom/client';
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
