6a3ac5ea1d29be1bf591c3c7ebb119aae3575d1e
Ryan Ford <59795228+ryanmford@users.noreply.github.com>
2026-04-14 20:15:49 -1000

    Update index.js

--- index.js
+++ index.js
@@ -3560,3 +3560,15 @@ export default function App() {
     </div>
   );
 }
+                          // --- MOUNTING ---
+import ReactDOM from 'react-dom/client';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
