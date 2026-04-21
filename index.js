414c56b77253901fe6123d9bce20988c65c3abec
Ryan Ford <59795228+ryanmford@users.noreply.github.com>
2026-04-20 14:23:26 -1000

    Update index.js

--- index.js
+++ index.js
@@ -701,7 +701,7 @@ const CustomStyles = React.memo(() => (
     .animate-marquee {
       display: flex;
       width: max-content;
-      animation: marquee 160s linear infinite;
+      animation: marquee 220s linear infinite;
     }
     .animate-marquee:hover { animation-play-state: paused; }
     
